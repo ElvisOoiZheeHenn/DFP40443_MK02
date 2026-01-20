d7857edff67a84ac3af8a7a02f31bcfd5e479120
Elvis Ooi Zhee Henn <elvisooi1025@gmail.com>
2026-01-20 14:22:37 +0800

    Aktiviti minggu lepas

---
+++ DIT4C 18DIT24F1164 ELVIS OOI ZHEE HENN/index.php
@@ -0,0 +1,22 @@
+<?php
+$nama = "ELVIS OOI ZHEE HENN";
+$nomatrik = "18DIT24F1164";
+$kelas = "DIT4C";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Diri</h1>
+    
+
+    <p>Nama: <?= $nama ?></p>
+    <p>No. Matrik: <?= $nomatrik ?></p>
+    <p>Kelas: <?= $kelas ?></p>
+
+    <img src="profile.jpg" alt="Foto Saya" width="200">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "KANG WEI JIAN";      // 改成你的全名（大写）
+$nomatrik = "18DDT23F1023";  // 改成你的学号
+$kelas = "DDT6A";            // 改成你的班级
+?>
